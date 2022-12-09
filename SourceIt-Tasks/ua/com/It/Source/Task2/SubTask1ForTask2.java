6e0c1c9e97a92bdd194e3a04d47d413eaa00d758
RiDiVoVa <ridivova8@gmail.com>
2022-12-09 15:50:28 +0300

    given my Subtask2ForTAsk2 and fix demo file

--- SourceIt-Tasks/ua/com/It/Source/Task2/SubTask1ForTask2.java
+++ SourceIt-Tasks/ua/com/It/Source/Task2/SubTask1ForTask2.java
@@ -1,30 +1,45 @@
 
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Stack;
 
 public class SubTask1ForTask2 {
-    public static boolean testString(String text) {
-        if (text == null)
-            throw new IllegalArgumentException();
-
-        Stack<Character> stack = new Stack<Character>();
-
-        for (int i = 0; i < text.length(); i++) {
-            char symbol = text.charAt(i);
-            if (symbol == '(' || symbol == '{' || symbol == '[')
-                stack.push(symbol);
-            else if (symbol == ']') {
-                if (stack.empty() || stack.pop() != '[')
-                    return false;
-            } else if (symbol == '}') {
-                if (stack.empty() || stack.pop() != '{')
-                    return false;
-            } else if (symbol == ')') {
-                if (stack.empty() || stack.pop() != '(')
-                    return false;
+    public static boolean truth;
+    public static boolean testString(String input) {
+        ArrayList<String> charsinputArray = new ArrayList<>(List.of(input.split("")));
+        for (int i = 0; i < charsinputArray.size(); i++) {
+            if (charsinputArray.get(i).equals("("))
+            {
+                truth = answerCloseBracets(")",charsinputArray);
+                charsinputArray.set(i,"1");
+            }
+            if (charsinputArray.get(i).equals("["))
+            {
+                truth = answerCloseBracets("]",charsinputArray);
+                charsinputArray.set(i,"1");
+            }
+            if (charsinputArray.get(i).equals("{"))
+            {
+                truth = answerCloseBracets("}",charsinputArray);
+                charsinputArray.set(i,"1");
+            }
+            if (!truth){break;}
+        }
+        return truth;
+    }
+    public static boolean answerCloseBracets(String nested,ArrayList<String> k){
+        boolean answerForBracets = false;
+        for (int i = 0; i < k.size(); i++)
+        {
+            if (k.get(i).equals(nested))
+            {
+                k.remove(i);
+                answerForBracets = true;
+                break;
             }
         }
-        return stack.empty();
+        return answerForBracets;
     }
-
 }
+
