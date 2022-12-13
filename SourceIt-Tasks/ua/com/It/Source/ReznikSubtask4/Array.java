a810804df3fece76efa6f5a2786919205dff54e8
RiDiVoVa <ridivova8@gmail.com>
2022-12-13 21:43:55 +0300

    done all of tasks from task2

--- SourceIt-Tasks/ua/com/It/Source/ReznikSubtask4/Array.java
+++ SourceIt-Tasks/ua/com/It/Source/ReznikSubtask4/Array.java
@@ -1,6 +1,7 @@
 public interface Array extends Container {
-    void add(Object element);
-    void set(int index,Object element);
+    String toString();
+    void add(Object element, int i);
+    void set(int index, Object element);
     Object get(int index);
     int indexOf(Object element);
     void remove(int index);
--- SourceIt-Tasks/ua/com/It/Source/ReznikSubtask4/Arraylmpl.java
+++ SourceIt-Tasks/ua/com/It/Source/ReznikSubtask4/Arraylmpl.java
@@ -1,14 +1,80 @@
 import java.util.Iterator;
-class Arraylmpl implements Array {
-    public Object[]array = new Object[10];
+
+public class Arraylmpl implements Array {
+    public Object[]array = new Object[3];
+    public static void main(String[] args) {
+        Arraylmpl m = new Arraylmpl();
+        m.add("A",0);
+        m.add("B",1);
+        m.add("C",2);
+
+        Iterator<Object> b = m.iterator();
+        while (b.hasNext()){
+            System.out.print(b.next() + " ");
+        }
+        System.out.println();
+        System.out.println("-----------------");
+
+
+        b.remove();
+        System.out.println("remove");
+        System.out.println(m.toString());
+        System.out.println("-----------------");
+
+        //set positions of elements
+        m.set(0,"C");
+        m.set(1,"B");
+        m.set(2,"A");
+
+        System.out.println(m.toString());
+        System.out.println("-----------------");
+
+        System.out.println("get Obj by index");
+        System.out.println(m.get(1));
+        System.out.println("-----------------");
+
+        System.out.println("indexOf");
+        System.out.println(m.indexOf("B"));
+        System.out.println("-----------------");
+
+        m.remove(1);
+        System.out.println("remove by index");
+        System.out.println(m.toString());
+        System.out.println("-----------------");
+
+        m.clear();
+        System.out.println("clear");
+        System.out.println(m.toString());
+        System.out.println("-----------------");
+
+        System.out.println("size");
+        System.out.println(m.size());
+        System.out.println("-----------------");
+
+    }
+
+   @Override
+    public String toString(){
+
+       StringBuilder strB = new StringBuilder();
+       for (int i = 0; i < array.length; i++) {
+           strB.append(array[i]);
+           strB.append(",");
+       }
+       strB.deleteCharAt(strB.length()-1);
+       String v = "["+ strB.toString()+"]";
+       return v;
+   }
 
     @Override
-    public void add(Object element) {
-        array[10] = element;
+    public void add( Object element,int i) {
+
+        array[i] = element;
     }
 
     @Override
     public void set(int index, Object element) {
+
         array[index] = element;
     }
 
@@ -28,55 +94,45 @@ class Arraylmpl implements Array {
 
     @Override
     public void remove(int index) {
-
-    }
-//TODO
-//    @Override
-//    public void remove(int index) {
-//
-//
-//    }
-
-    class Iteratorlmpl implements java.util.Iterator<Object> {
-        @Override
-        public boolean hasNext() {
-
-            return false;
-        }
-
-        @Override
-        public Object next() {
-            return null;
-        }
-
-        @Override
-        public void remove(){
-            iterator().remove();
-        }
+        array[index] = null;
     }
 
     @Override
     public void clear() {
-
+        for (int i = 0; i < array.length; i++) {
+            array[i] = null;
+        }
     }
 
     @Override
     public int size() {
-        return 0;
+        return array.length;
     }
 
+    @Override
     public Iterator<Object> iterator(){
         return new Iteratorlmpl();
     }
 
+    public class Iteratorlmpl implements java.util.Iterator<Object> {
+       private int i = 0;
 
-    public void main(String[] args) {
+        @Override
+        public boolean hasNext() {
+            return array.length>i;
+        }
 
+        @Override
+        public Object next() {
+            return String.valueOf(array[i++]);
+        }
 
+        @Override
+        public void remove(){
+            for (int j = 0; j < array.length; j++) {
+                array[j] = null;
+            }
+
+        }
     }
 }
-
-
-
-
-
